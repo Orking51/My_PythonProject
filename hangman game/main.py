9e543fabf014cb4d032d0d3ae545f65319fa67e5
Orking51 <139702858+Orking51@users.noreply.github.com>
2023-11-26 19:01:23 +0100

    Add files via upload

---
+++ hangman game/main.py
@@ -0,0 +1,60 @@
+import random
+import linecache
+
+
+def restart():
+    if (input("voulez vous recommancer? o/n: ")) == "o":
+        game()
+    else:
+        quit()
+
+def game():
+    
+    n = 0
+    i= 10
+    word = linecache.getline('French_WordBook.txt', random.randint(1, 22735)).strip()
+    f = ""
+    test = ""
+    user_input = input("les noirs dehors: ")
+    for x in range(len(word)):
+        if user_input == word[x]:
+            test += user_input
+            n+=1
+        else:
+            test += "_"
+            
+    if n<1:
+        print("il vous reste plus que",i, "erreurs") 
+        i -= 1
+        
+    print(test)
+    caracteres = list(test)
+
+    while i>0:
+        n=0
+        if i == 0:
+            print("vous etes nul, vous etes une grosse merde, le mot etait: ", word)
+        user_input = input("les noirs dehors: ")
+        for x in range(len(word)):
+            if user_input == word[x]:   
+                caracteres[x] = user_input
+                test = "".join(caracteres)
+                n+=1
+        if n<1:
+            print("il vous reste plus que",i, "erreurs") 
+            i -= 1
+        print(test) 
+        if word == test:
+            print("vous avez gagné")
+            restart()
+
+    print("vous etes nul, vous etes une grosse merde, le mot etait: ", word)
+    restart()
+    
+game()
+
+
+        
+
+
+  
